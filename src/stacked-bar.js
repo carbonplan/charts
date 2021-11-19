c6f20afef5cdbcaa7a8c29632ecaf97be22c01e5
Kata Martin <katasm@gmail.com>
2021-11-19 11:05:23 -0800

    Complete bar color logic

--- src/stacked-bar.js
+++ src/stacked-bar.js
@@ -1,15 +1,53 @@
-import React, { memo } from 'react'
+import React, { memo, useMemo } from 'react'
 import { scaleLinear } from 'd3-scale'
 
 import Bar from './bar'
 
-const StackedBar = ({
-  data,
-  color = 'primary',
-  range = [0.3, 0.9],
-  sx,
-  ...props
-}) => {
+const getBarColors = (color, barLength, dataLength, opacityRange) => {
+  let colors
+  let opacities = []
+  if (typeof color === 'string') {
+    // single color for all bars has been provided, use same color with different opacity per bar
+    colors = new Array(barLength).fill(color)
+    const opacity = scaleLinear()
+      .domain([barLength - 1, 0])
+      .range(opacityRange)
+    opacities = new Array(barLength).fill(null).map((_, i) => opacity(i))
+  } else if (color.every((c) => typeof c === 'string')) {
+    // color has been specified for each bar
+    if (color.length !== barLength) {
+      throw new Error(
+        `Unexpected 1D color array provided. Expected length equal to number of bars: ${barLength}, received length: ${color.length}`
+      )
+    }
+    colors = color
+  } else {
+    // color has been specified for each datum
+    if (color.length !== dataLength) {
+      throw new Error(
+        `Unexpected 2D color array provided. Expected length equal to data: ${dataLength}, received length: ${color.length}`
+      )
+    }
+    const invalidSubarray = color.find((c) => c.length !== barLength)
+    if (invalidSubarray) {
+      throw new Error(
+        `Unexpected 2D color array provided. Expected all subarrays to have length equal to number of bars: ${barLength}, received length: ${invalidSubarray.length}`
+      )
+    }
+
+    colors = color.reduce(
+      (accum, datum) => {
+        datum.forEach((barColor, i) => accum[i].push(barColor))
+        return accum
+      },
+      new Array(barLength).fill(null).map(() => [])
+    )
+  }
+
+  return { colors, opacities }
+}
+
+const StackedBar = ({ data, color = 'primary', range, sx, ...props }) => {
   const stackedData = data[0].slice(2).map(() => [])
   const bars = data.reduce((accum, datum) => {
     const [x, ...yValues] = datum
@@ -30,29 +68,27 @@ const StackedBar = ({
     return accum
   }, stackedData)
 
-  if (Array.isArray(color) && color.length !== stackedData.length) {
-    throw new Error(
-      `Unexpected color array provided. Expected length ${stackedData.length}, received length ${color.length}`
-    )
-  }
-  const opacity = scaleLinear()
-    .domain([bars.length - 1, 0])
-    .range(range)
+  const { colors, opacities } = useMemo(
+    () => getBarColors(color, bars.length, data.length, range || [0.3, 0.9]),
+    [color, bars.length, data.length, range]
+  )
 
   return (
     <>
-      {bars.map((bar, i) => (
-        <Bar
-          {...props}
-          key={i}
-          data={bar}
-          color={typeof color === 'string' ? color : color[i]}
-          sx={{
-            fillOpacity: typeof color === 'string' ? opacity(i) : undefined,
-            ...sx,
-          }}
-        />
-      ))}
+      {bars.map((bar, i) => {
+        return (
+          <Bar
+            {...props}
+            key={i}
+            data={bar}
+            color={colors[i]}
+            sx={{
+              fillOpacity: opacities[i],
+              ...sx,
+            }}
+          />
+        )
+      })}
     </>
   )
 }
