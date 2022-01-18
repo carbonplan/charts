78f0edc794a36978be1dfb037765d181c6de8f84
freeman-lab <the.freeman.lab@gmail.com>
2022-01-18 00:08:58 -0800

    support label alignment

--- src/label.js
+++ src/label.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Box } from 'theme-ui'
 import Point from './point'
 
-const Label = ({ x, y, children, sx }) => {
+const Label = ({ x, y, children, align='left', verticalAlign='top', width, sx }) => {
   return (
-    <Point x={x} y={y}>
+    <Point x={x} y={y} width={width} align={align} verticalAlign={verticalAlign}>
       <Box
         sx={{
           fontFamily: 'mono',
@@ -12,6 +12,7 @@ const Label = ({ x, y, children, sx }) => {
           textTransform: 'uppercase',
           fontSize: [0],
           color: 'secondary',
+          textAlign: align,
           ...sx,
         }}
       >
--- src/point.js
+++ src/point.js
@@ -1,8 +1,53 @@
 import React from 'react'
 import { useChart } from './chart'
 
-const Point = ({ x, y, children }) => {
+const Point = ({ x, y, children, align='left', verticalAlign='top', width }) => {
   const { x: _x, y: _y, pl, pr, pt, pb, apl, apr, apt, apb } = useChart()
+
+  let position, verticalPosition
+
+  if (!['left', 'right', 'center'].includes(align)) {
+    throw new Error(`'${align}' is not a recognized alignment, must be left, right, or center`)
+  }
+
+  if (!['top', 'bottom'].includes(verticalAlign)) {
+    throw new Error(`'${verticalAlign}' is not a recognized vertical alignment, must be top or bottom`)
+  }
+
+  if (align === 'center' && !width) {
+    throw new Error(`center alignment requires specifying a width`)
+  }
+
+  if (align === 'left') {
+    position = {
+      left: `${_x(x)}%`,
+    }
+  }
+
+  if (align === 'center') {
+    position = {
+      left: `${_x(x - width/2)}%`,
+      right: `${100 - _x(x + width/2)}%`,
+    }
+  }
+
+  if (align === 'right') {
+    position = {
+      right: `${100 - _x(x)}%`,
+    }
+  }
+
+  if (verticalAlign === 'top') {
+    verticalPosition = {
+      top: `${_y(y)}%`,
+    }
+  }
+
+  if (verticalAlign === 'bottom') {
+    verticalPosition = {
+      bottom: `${100 - _y(y)}%`,
+    }
+  }
 
   return (
     <div
@@ -17,8 +62,8 @@ const Point = ({ x, y, children }) => {
       <div
         style={{
           position: 'absolute',
-          top: `${_y(y)}%`,
-          left: `${_x(x)}%`,
+          ...position,
+          ...verticalPosition
         }}
       >
         {children}
