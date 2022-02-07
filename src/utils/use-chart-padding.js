da226b07e470e010595010bc3d1f42b2b98eea5c
Kata Martin <katasm@gmail.com>
2022-02-07 09:19:28 -0800

    Only generate responsive styles when multiple padding values are used

--- src/utils/use-chart-padding.js
+++ src/utils/use-chart-padding.js
@@ -3,40 +3,44 @@ import { useChart } from '../chart'
 const KEYS = ['pl', 'pr', 'pt', 'pb', 'apl', 'apr', 'apt', 'apb']
 
 const getChartPadding = (functionalSx, rawValues) => {
-  const breakpointValues = KEYS.reduce(
-    (accum, key) => {
-      const rawValue = rawValues[key]
-      let arrayValue
-      if (Array.isArray(rawValue)) {
-        if (rawValue.length === 4) {
-          arrayValue = rawValue
+  if (KEYS.some((key) => Array.isArray(rawValues[key]))) {
+    const breakpointValues = KEYS.reduce(
+      (accum, key) => {
+        const rawValue = rawValues[key]
+        let arrayValue
+        if (Array.isArray(rawValue)) {
+          if (rawValue.length === 4) {
+            arrayValue = rawValue
+          } else {
+            arrayValue = new Array(4)
+              .fill(null)
+              .map((_, i) => rawValue[i] ?? rawValue[rawValue.length - 1])
+          }
         } else {
-          arrayValue = new Array(4)
-            .fill(null)
-            .map((_, i) => rawValue[i] ?? rawValue[rawValue.length - 1])
+          arrayValue = new Array(4).fill(rawValue)
         }
-      } else {
-        arrayValue = new Array(4).fill(rawValue)
-      }
-      arrayValue.forEach((value, i) => {
-        accum[i][key] = value
+        arrayValue.forEach((value, i) => {
+          accum[i][key] = value
+        })
+
+        return accum
+      },
+      [{}, {}, {}, {}]
+    )
+
+    const sxKeys = Object.keys(functionalSx({}))
+
+    return breakpointValues.reduce((accum, values, i) => {
+      const result = functionalSx(values)
+      sxKeys.map((key) => {
+        accum[key] ||= []
+        accum[key].push(result[key])
       })
-
       return accum
-    },
-    [{}, {}, {}, {}]
-  )
-
-  const sxKeys = Object.keys(functionalSx({}))
-
-  return breakpointValues.reduce((accum, values, i) => {
-    const result = functionalSx(values)
-    sxKeys.map((key) => {
-      accum[key] ||= []
-      accum[key].push(result[key])
-    })
-    return accum
-  }, {})
+    }, {})
+  } else {
+    return functionalSx(rawValues)
+  }
 }
 
 const useChartPadding = (functionalSx) => {
