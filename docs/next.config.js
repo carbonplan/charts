5dff73878e2d7679c9864411360b547dd5981b46
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-03-14 15:08:55 -0500

    update dep so new charts version can be used

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,26 +1,7 @@
-const path = require('path')
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
+  options: { format: 'mdx' },
 })
-
 module.exports = withMDX({
-  pageExtensions: ['js', 'jsx', 'mdx', 'md'],
-  webpack: (config, options) => {
-    if (options.isServer) {
-      config.externals = ['react', 'theme-ui', ...config.externals]
-    }
-    config.resolve.alias['react'] = path.resolve(
-      __dirname,
-      '.',
-      'node_modules',
-      'react'
-    )
-    config.resolve.alias['theme-ui'] = path.resolve(
-      __dirname,
-      '.',
-      'node_modules',
-      'theme-ui'
-    )
-    return config
-  },
+  pageExtensions: ['js', 'jsx', 'tsx', 'md', 'mdx'],
 })
