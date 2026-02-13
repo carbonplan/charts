b237a46b9b5088e8ac1282cfa576ffd1bf728441
Shane Loeffler <Shane98c@users.noreply.github.com>
2026-02-13 17:36:07 -0600

    add discriminated union types for Scatter data

--- src/scatter.tsx
+++ src/scatter.tsx
@@ -3,12 +3,32 @@ import { BoxProps } from 'theme-ui'
 import { useChart } from './chart'
 import { PathBox } from './svg'
 
-export interface ScatterProps extends Omit<BoxProps, 'color'> {
-  data: number[][]
-  x?: (d: number[]) => number
-  y?: (d: number[]) => number
+type ScatterTuple = readonly number[]
+type ScatterObject = Record<string, number>
+type ScatterDatum = ScatterTuple | ScatterObject
+type ScatterBaseProps<T extends ScatterDatum> = Omit<BoxProps, 'color'> & {
+  data: T[]
   color?: string
   size?: number
+}
+
+export type ScatterTupleProps = ScatterBaseProps<ScatterTuple> & {
+  x?: (d: ScatterTuple) => number
+  y?: (d: ScatterTuple) => number
+}
+
+export type ScatterObjectProps = ScatterBaseProps<ScatterObject> & {
+  x: (d: ScatterObject) => number
+  y: (d: ScatterObject) => number
+}
+
+export type ScatterProps = ScatterTupleProps | ScatterObjectProps
+
+const tupleCoordinate = (d: ScatterDatum, index: 0 | 1) => {
+  if (Array.isArray(d) && typeof d[index] === 'number') return d[index]
+  throw new Error(
+    'Scatter: default accessors require tuple data like [x, y]. For object data, pass both x and y accessors.'
+  )
 }
 
 const Scatter = ({
@@ -21,10 +41,16 @@ const Scatter = ({
   ...props
 }: ScatterProps) => {
   const { x: _x, y: _y } = useChart()
-  x = x || ((d) => d[0])
-  y = y || ((d) => d[1])
+  const xAccessor =
+    (x as ((d: ScatterDatum) => number) | undefined) ||
+    ((d: ScatterDatum) => tupleCoordinate(d, 0))
+  const yAccessor =
+    (y as ((d: ScatterDatum) => number) | undefined) ||
+    ((d: ScatterDatum) => tupleCoordinate(d, 1))
 
-  const path = data.map((d) => `M${_x(x(d))},${_y(y(d))} l0.01,0.01`).join(' ')
+  const path = data
+    .map((d) => `M${_x(xAccessor(d))},${_y(yAccessor(d))} l0.01,0.01`)
+    .join(' ')
 
   return (
     <PathBox
