33b749c4dfcb90b6ccdc969740335090fd4e9e6e
Shane Loeffler <Shane98c@users.noreply.github.com>
2026-02-18 11:03:00 -0600

    PathBox for Rect and cleanup mutation

--- src/rect.tsx
+++ src/rect.tsx
@@ -1,36 +1,34 @@
 import React, { memo } from 'react'
-import { get, useThemeUI } from 'theme-ui'
+import { BoxProps } from 'theme-ui'
 import { useChart } from './chart'
+import { PathBox } from './svg'
 
-export interface RectProps
-  extends Omit<React.SVGProps<SVGPathElement>, 'x' | 'y'> {
+export interface RectProps extends Omit<BoxProps, 'color'> {
   x: [number, number]
   y: [number, number]
   color?: string
 }
 
-const Rect = ({ x, y, color = 'primary', ...props }: RectProps) => {
+const Rect = ({ x, y, color = 'primary', sx, ...props }: RectProps) => {
   const { x: _x, y: _y } = useChart()
-  const { theme } = useThemeUI()
 
-  if (x[0] > x[1]) {
-    x = [x[1], x[0]]
-  }
+  const sortedX: [number, number] = x[0] > x[1] ? [x[1], x[0]] : x
+  const sortedY: [number, number] = y[0] > y[1] ? [y[1], y[0]] : y
 
-  if (y[0] > y[1]) {
-    y = [y[1], y[0]]
-  }
+  const width = Math.abs(sortedX[1] - sortedX[0])
+  const height = Math.abs(sortedY[1] - sortedY[0])
 
-  const width = Math.abs(x[1] - x[0])
-  const height = Math.abs(y[1] - y[0])
+  const h = _x(sortedX[0] + width) - _x(sortedX[0])
+  const v = _y(sortedY[0]) - _y(sortedY[0] + height)
 
-  const h = _x(x[0] + width) - _x(x[0])
-  const v = _y(y[0]) - _y(y[0] + height)
   return (
-    <path
-      d={`M ${_x(x[0])} ${_y(y[1])} h ${h} v ${v} h -${h} Z`}
-      fill={get(theme, `rawColors.${color}`, color)}
-      stroke='none'
+    <PathBox
+      d={`M ${_x(sortedX[0])} ${_y(sortedY[1])} h ${h} v ${v} h -${h} Z`}
+      sx={{
+        fill: color,
+        stroke: 'none',
+        ...sx,
+      }}
       {...props}
     />
   )
