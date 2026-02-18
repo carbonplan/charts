241c9455511df57f111aa5c10b9f48d548607434
Shane Loeffler <Shane98c@users.noreply.github.com>
2026-02-18 10:59:07 -0600

    widen svg wrapper types

--- src/svg.tsx
+++ src/svg.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Box, BoxProps } from 'theme-ui'
 
-type PathBoxProps = BoxProps & { d?: string | null }
+type PathBoxProps = BoxProps &
+  Omit<React.SVGProps<SVGPathElement>, keyof BoxProps | 'd'> & {
+    d?: string | null
+  }
 export const PathBox = (props: PathBoxProps) => <Box as='path' {...props} />
 
 type SvgBoxProps = BoxProps &
-  Pick<
-    React.SVGProps<SVGSVGElement>,
-    'viewBox' | 'preserveAspectRatio' | 'height' | 'width'
-  >
+  Omit<React.SVGProps<SVGSVGElement>, keyof BoxProps>
 export const SvgBox = (props: SvgBoxProps) => <Box as='svg' {...props} />
 
 type LineBoxProps = BoxProps &
-  Pick<React.SVGProps<SVGLineElement>, 'x1' | 'x2' | 'y1' | 'y2'>
+  Omit<React.SVGProps<SVGLineElement>, keyof BoxProps>
 export const LineBox = (props: LineBoxProps) => <Box as='line' {...props} />
